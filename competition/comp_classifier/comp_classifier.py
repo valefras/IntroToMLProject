7ce584fd4ef71f1592db0b6859abf8ba7b89ddc0
Lorenzo Bocchi <lorenzobocchi99@yahoo.com>
2022-04-29 20:35:01 +0200

    edited convolution, more epochs

--- competition/comp_classifier/comp_classifier.py
+++ competition/comp_classifier/comp_classifier.py
@@ -31,29 +31,26 @@ def configure_subparsers(subparsers):
 class comp_classifier(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = torch.nn.Conv2d(3, 5, 9)
+        self.conv1 = torch.nn.Conv2d(3, 5, 5)
         self.pool1 = torch.nn.MaxPool2d(2, 2)
         self.conv2 = torch.nn.Conv2d(5, 10, 5)
         self.pool2 = torch.nn.MaxPool2d(2, 2)
-        self.conv3 = torch.nn.Conv2d(10, 5, 5)
-        self.pool3 = torch.nn.MaxPool2d(2, 2)
+        self.conv3 = torch.nn.Conv2d(10, 15, 3)
         self.flatten = torch.nn.Flatten()
-        self.fc1 = torch.nn.Linear(2880, 1440)
-        self.fc2 = torch.nn.Linear(1440, 720)
-        self.fc3 = torch.nn.Linear(720, 200)
-        self.fc4 = torch.nn.Linear(200, 50)
-        self.fc5 = torch.nn.Linear(50, 10)
+        self.fc1 = torch.nn.Linear(1815, 900)
+        self.fc2 = torch.nn.Linear(900, 200)
+        self.fc3 = torch.nn.Linear(200, 100)
+        self.fc4 = torch.nn.Linear(100, 30)
 
     def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
-        x = self.pool3(F.relu(self.conv3(x)))
+        x = F.relu(self.conv3(x))
         x = self.flatten(x)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
         return x
 
 
@@ -81,7 +78,7 @@ def main(args):
         net.parameters(), lr=lr, weight_decay=1e-8)
 
     model_transform = tv.transforms.Compose([
-        tv.transforms.Resize((224, 224)),
+        tv.transforms.Resize((64, 64)),
         tv.transforms.ColorJitter(hue=.05, saturation=.05),
         tv.transforms.RandomHorizontalFlip(),
         tv.transforms.RandomRotation(20, resample=PIL.Image.BILINEAR),
@@ -90,7 +87,7 @@ def main(args):
     ])
 
     model = Competition_classifier(
-        net, optimizer, loss_function, model_transform, "comp_classifier", "animals", 10)
+        net, optimizer, loss_function, model_transform, "comp_classifier", "animals", 175)
 
     if(args.test != None):
         path_model = utils.get_path(
